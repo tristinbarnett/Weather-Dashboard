60d1903415c5b9e742aa95102883e4b331165e99
Tristin Barnett <tristinbarnett@gmail.com>
2020-02-26 15:51:57 -0800

    linked in weather API that searches based on city, console logged response from apex call

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-$("#searchCity").click(function(event){
-   var city =  $("#city").val();
-   console.log(city);
-   var cityButton = $("<a>").attr({
-       class: "list-group-item list-group-item-action",
-       href: "#"
-   });
-   cityButton.text(city);
-   $(".list-group").append(cityButton);
+var city;
+
+
+$("#searchCity").click(function (event) {
+    city = $("#city").val();
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=6ae2d75d12546b3e67c1a101fdff49bc"
+
+    var cityButton = $("<a>").attr({
+        class: "list-group-item list-group-item-action",
+        href: "#"
+    });
+    cityButton.text(city);
+    $(".list-group").append(cityButton);
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+    })
 })
