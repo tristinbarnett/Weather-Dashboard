d6c1d30ffeadb9847f441930cfab2b82ad84d5f7
Tristin Barnett <tristinbarnett@gmail.com>
2020-02-29 19:49:28 -0800

    added dates to 5day and added image

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 var city;
 var cardBody = $(".card-body");
-var cardTitle = $(".card-title");
 var searchHistory = [];
 
 function getItems() {
@@ -9,7 +8,7 @@ function getItems() {
     if (storedCities !== null) {
         searchHistory = storedCities;
     };
-    
+
     for (i = 0; i < searchHistory.length; i++) {
         cityButton = $("<a>").attr({
             class: "list-group-item list-group-item-action",
@@ -21,33 +20,23 @@ function getItems() {
 };
 getItems();
 
-$("#searchCity").click(function (event) {
-
-    city = $("#city").val();
-    searchHistory.push(city);
-    localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
-
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=6ae2d75d12546b3e67c1a101fdff49bc"
-
-    var cityButton = $("<a>").attr({
-        class: "list-group-item list-group-item-action",
-        href: "#"
-    });
-    cityButton.text(city);
-    $(".list-group").append(cityButton);
-
+function getData() {
+    var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=6ae2d75d12546b3e67c1a101fdff49bc`
+    cardBody.empty();
+    $("#weeklyForecast").empty();
 
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
         var date = moment().format("(MM/DD/YYYY)");
-        var name = $("<h3>").text(city + date);
-        cardTitle.prepend(name);
-
         var iconCode = response.weather[0].icon;
         var iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png";
-        cardTitle.append($("<img>").attr("src", iconURL));
+        var name = $("<h3>").html(city + date);
+        cardBody.prepend(name);
+        cardBody.append($("<img>").attr("src", iconURL));
+
+    
 
         var temp = Math.round((response.main.temp - 273.15) * 1.80 + 32);
         cardBody.append($("<p>").html("Temperature: " + temp + " &#176;F"));
@@ -86,24 +75,51 @@ $("#searchCity").click(function (event) {
         }).then(function (response) {
             console.log(response);
             for (i = 0; i < 5; i++) {
+
                 var newColumn = $("<div>").attr("class", "col fiveDay bg-primary text-white");
                 $("#weeklyForecast").append(newColumn);
 
-                var iconCode = response.list[i].weather[0].icon;
+                var myDate = new Date(response.list[i*8].dt * 1000);
+                console.log(myDate.toLocaleDateString());
+                newColumn.append($("<h4>").html(myDate.toLocaleDateString()));
+
+                var iconCode = response.list[i*8].weather[0].icon;
                 var iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png";
                 newColumn.append($("<img>").attr("src", iconURL));
 
-                var temp = Math.round((response.list[i].main.temp - 273.15) * 1.80 + 32);
+                var temp = Math.round((response.list[i*8].main.temp - 273.15) * 1.80 + 32);
                 newColumn.append($("<p>").html("Temp: " + temp + " &#176;F"));
 
-                var humidity = response.list[i].main.humidity;
+                var humidity = response.list[i*8].main.humidity;
                 newColumn.append($("<p>").html("Humidity: " + humidity + "&#37;"));
 
-                var myDate = new Date(response.list[i].dt * 1000);
-                console.log(myDate.toLocaleString());
             }
         })
     })
+};
+$("#searchCity").click(function (event) {
+    city = $("#city").val();
+    getData();
+    var checkArray = searchHistory.includes(city);
+    if (checkArray == true) {
+        return
+    }
+    else {
+        searchHistory.push(city);
+        localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+        var cityButton = $("<a>").attr({
+            class: "list-group-item list-group-item-action",
+            href: "#"
+        });
+        cityButton.text(city);
+        $(".list-group").append(cityButton);
+    };
+});
+
+$(".list-group-item").click(function (event) {
+    city = $(this).text();
+    getData();
+});
 
 
-})
+
